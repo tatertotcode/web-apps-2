35e56051046b559d79f251030da3fa33b730c854
tatertotcode <elijah.kravets@kellogg.northwestern.edu>
2026-02-17 17:02:49 +0000

    crm view companies, add new company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,11 +1,18 @@
 class CompaniesController < ApplicationController
 
   def index
+    @companies = Company.all 
     # find all Company rows
     # render companies/index view
   end
 
   def show
+    # puts "--------------"
+    # params["id"]
+    # puts "--------------"
+    @company = Company.find_by({"id" => params["id"]})
+    p @company
+
     # find a Company
     # render companies/show view with details about Company
   end
@@ -14,12 +21,21 @@ class CompaniesController < ApplicationController
     # render view with new Company form
   end
 
-  # def create
-  #   # start with a new Company
-  #   # assign user-entered form data to Company's columns
-  #   # save Company row
-  #   # redirect user
-  # end
+  def create
+    @company = Company.new(company_params)
+    if @company.save
+      redirect_to "/companies"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :city, :state, :url)
+  end
+
 
   # def edit
   #   # find a Company
